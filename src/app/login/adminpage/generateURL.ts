86218359fa5b1d9701420343ddaddbc35e9cca39
ArneeMe <42463181+ArneeMe@users.noreply.github.com>
2025-01-17 16:06:26 +0100

    Update URL path to be generic

--- src/app/login/adminpage/generateURL.ts
+++ src/app/login/adminpage/generateURL.ts
@@ -2,6 +2,7 @@ import {Volunteer} from "@/app/util/Volunteer";
 import {generateParams} from "@/app/login/adminpage/generateParams";
 
 export const generateURL = (formData: Volunteer): string => {
-    const prefix = "attester.no/verify"
-    return `${prefix}?${generateParams(formData)}`;
+    const baseUrl = `${window.location.protocol}//${window.location.host}`;
+    const path = 'verify'
+    return `${baseUrl}/${path}?${generateParams(formData)}`;
 };
